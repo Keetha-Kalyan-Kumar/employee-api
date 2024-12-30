67e9b309f96a6bbe7e353c780c8e0b0d23e74d83
keethakalyankumar <keethakalyankumar@localhost>
2024-12-30 21:58:13 +0530

    Added the comment

--- fetch-employees/src/main/java/com/example/fetch_employees/FetchEmployeesApplication.java
+++ fetch-employees/src/main/java/com/example/fetch_employees/FetchEmployeesApplication.java
@@ -1,3 +1,4 @@
+
 package com.example.fetch_employees;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FetchEmployeesApplication {
 
+	
+	//Comment
 	public static void main(String[] args) {
 		SpringApplication.run(FetchEmployeesApplication.class, args);
 	}
